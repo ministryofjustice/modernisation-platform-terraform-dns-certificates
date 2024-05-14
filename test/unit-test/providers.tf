f03530f698ca6e1eb72f2fa0bae41b1927d13d9c
ep-93 <109581241+ep-93@users.noreply.github.com>
2024-05-14 14:45:20 +0100

    Update providers.tf

--- test/unit-test/providers.tf
+++ test/unit-test/providers.tf
@@ -12,12 +12,12 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-#AWS provider for core-vpc-<environment>, to share VPCs into this account
+# AWS provider for core-vpc-<environment>, to share VPCs into this account
 provider "aws" {
   alias  = "core-vpc"
   region = "eu-west-2"
   assume_role {
-    role_arn = "arn:aws:iam::${local.environment_management.account_ids[local.provider_name]}:role/member-delegation-${local.vpc_name}-${local.app_name}"
+    role_arn = "arn:aws:iam::${local.environment_management.account_ids[local.provider_name]}:role/member-delegation-${local.vpc_name}-${local.environment}"
   }
 }
 

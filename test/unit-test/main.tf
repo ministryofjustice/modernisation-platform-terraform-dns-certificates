64d2da6d8bddc1f5c23e98aee9c11b76cb9a7d5e
Aaron Robinson <aaron.robinson@digital.justice.gov.uk>
2024-05-16 16:12:18 +0100

    providers

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -1,5 +1,8 @@
 module "cert_module" {
   source         = "../../"
+  providers      = {
+    aws.core-network-services = aws.core-network-services
+  }
   aws_account_id = local.environment_management.account_ids
   record_type    = "CNAME"
   fqdn           = "platforms-test.modernisation-platform.service.justice.gov.uk"

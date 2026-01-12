2f566ed3c42ea5f00584eab9d9d9f0895590cae9
mikereiddigital <mike.reid@justice.gov.uk>
2026-01-12 11:04:47 +0000

    Amended assertions

--- test/module_test.go
+++ test/module_test.go
@@ -20,29 +20,36 @@ func TestCertificateCreation(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Test certificate_domain output
-	certificateDomain := terraform.Output(t, terraformOptions, "certificate_domain")
-	assert.Equal(t, "platforms-test.modernisation-platform.service.justice.gov.uk", certificateDomain, "Certificate domain should match expected FQDN")
+	// Test non-production module outputs
+	nonProdCertificateDomain := terraform.Output(t, terraformOptions, "non_prod_certificate_domain")
+	assert.Equal(t, "platforms-test.modernisation-platform.service.justice.gov.uk", nonProdCertificateDomain, "Non-prod certificate domain should match expected FQDN")
 
-	// Test certificate_arn output
-	certificateArn := terraform.Output(t, terraformOptions, "certificate_arn")
-	assert.NotEmpty(t, certificateArn, "Certificate ARN should not be empty")
-	assert.True(t, strings.HasPrefix(certificateArn, "arn:aws:acm:"), "Certificate ARN should start with 'arn:aws:acm:'")
+	nonProdCertificateArn := terraform.Output(t, terraformOptions, "non_prod_certificate_arn")
+	assert.NotEmpty(t, nonProdCertificateArn, "Non-prod certificate ARN should not be empty")
+	assert.True(t, strings.HasPrefix(nonProdCertificateArn, "arn:aws:acm:"), "Non-prod certificate ARN should start with 'arn:aws:acm:'")
 
-	// Test certificate_id output
-	certificateId := terraform.Output(t, terraformOptions, "certificate_id")
-	assert.NotEmpty(t, certificateId, "Certificate ID should not be empty")
-
-	// Test certificate_domain_validation_options output
-	domainValidationOptions := terraform.Output(t, terraformOptions, "certificate_domain_validation_options")
-	assert.NotEmpty(t, domainValidationOptions, "Certificate domain validation options should not be empty")
-
-	// Test dns_validation_records_core_vpc output (should have records for non-prod)
-	dnsValidationRecordsCoreVpc := terraform.Output(t, terraformOptions, "dns_validation_records_core_vpc")
-	assert.NotEmpty(t, dnsValidationRecordsCoreVpc, "DNS validation records for core-vpc should not be empty for non-production")
+	// Test dns_validation_records_core_vpc output (should have records for non-prod, null for prod)
+	nonProdDnsValidationRecords := terraform.Output(t, terraformOptions, "non_prod_dns_validation_records")
+	assert.NotEmpty(t, nonProdDnsValidationRecords, "DNS validation records for core-vpc should not be empty for non-production")
 
 	// Test certificate_validation_non_prod output (should exist for non-prod)
-	certificateValidationNonProd := terraform.Output(t, terraformOptions, "certificate_validation_non_prod")
-	assert.NotEmpty(t, certificateValidationNonProd, "Certificate validation non-prod should not be empty for non-production")
+	nonProdCertificateValidation := terraform.Output(t, terraformOptions, "non_prod_certificate_validation")
+	assert.NotEmpty(t, nonProdCertificateValidation, "Certificate validation non-prod should not be empty for non-production")
+
+	// Test production module outputs
+	prodCertificateDomain := terraform.Output(t, terraformOptions, "prod_certificate_domain")
+	assert.Equal(t, "testing-test.modernisation-platform.service.justice.gov.uk", prodCertificateDomain, "Prod certificate domain should match expected FQDN")
+
+	prodCertificateArn := terraform.Output(t, terraformOptions, "prod_certificate_arn")
+	assert.NotEmpty(t, prodCertificateArn, "Prod certificate ARN should not be empty")
+	assert.True(t, strings.HasPrefix(prodCertificateArn, "arn:aws:acm:"), "Prod certificate ARN should start with 'arn:aws:acm:'")
+
+	// Test dns_validation_records_core_network_services output (should have records for prod, null for non-prod)
+	prodDnsValidationRecords := terraform.Output(t, terraformOptions, "prod_dns_validation_records")
+	assert.NotEmpty(t, prodDnsValidationRecords, "DNS validation records for core-network-services should not be empty for production")
+
+	// Test certificate_validation_prod output (should exist for prod)
+	prodCertificateValidation := terraform.Output(t, terraformOptions, "prod_certificate_validation")
+	assert.NotEmpty(t, prodCertificateValidation, "Certificate validation prod should not be empty for production")
 
 }
